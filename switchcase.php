aa9bcb581ab8ee01f32325e7ac77eec06dc4375f
Al-amintpi <alamincmt7418@gmail.com>
2020-10-29 12:27:36 +0600

    switchcase

--- switchcase.php
+++ switchcase.php
@@ -1,1 +1,19 @@
- 
+<?php
+	$number = 10;
+	switch ($number) {
+
+		case 10:
+			echo "the number is 10";
+			break;
+		
+		case 20:
+			echo "the number is 20";
+
+		case 30:
+			echo "the number is 30";	
+
+		default:
+			# code...
+			break;
+	}
+ ?>
